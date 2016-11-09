cb75acdddc1ff2660e8e1d317daa48ca2af6677a
Bryan Gronberg <btgr91@gmail.com>
2016-11-08 18:28:01 -0700

    Add files via upload

---
+++ file.js
@@ -0,0 +1,48 @@
+//Twitter Widgets Import------------------
+
+window.twttr = (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+        t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+
+    t._e = [];
+    t.ready = function(f) {
+        t._e.push(f);
+    };
+
+    return t;
+}(document, "script", "twitter-wjs"));
+
+
+//Button Logic--------------------
+
+var quoteText;
+var quoteAuthor;
+
+function updateTwitterValues(share_url, title) {
+    // clear out the <a> tag that's currently there...probably don't really need this since you're replacing whatever is in there already.
+    $("#twitter").html('&nbsp;');
+    $("#twitter").html('<a href="https://twitter.com/share" class="twitter-share-button" data-url="' + share_url + '" data-size="large" data-text="' + title + '" data-count="none">Tweet</a>');
+    twttr.widgets.load(document.getElementById("twitter"));
+}
+
+$(document).ready(function() {
+    $("#get-random-quote").click(function() {
+        var random = Math.floor(Math.random() * 30);
+        $.getJSON("https://crossorigin.me/http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=40&callback=", function(a) {
+            quoteText = a[random].content;
+            quoteAuthor = a[random].title;
+            $("#goes-here").html(quoteText + "<p>— " + quoteAuthor + "</p>");
+            updateTwitterValues(twitterUrl);
+        })
+    })
+});
+
+
+// var twitterUrl = "https://twitter.com/intent/tweet?text=Hello%20world";
+// var tweetText = "\"" + quoteText + "\"" + " - " + quoteAuthor;
+// twitterUrl += tweetText;
